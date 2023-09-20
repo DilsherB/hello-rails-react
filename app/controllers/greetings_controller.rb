3556454066397c8525d6fea230f7bd0d19b80bec
Dilsher Balouch <dilsherb@gmail.com>
2023-09-20 07:19:17 +0300

    linter configurations done

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random
-    greeting = Message.order("RANDOM()").first
+    greeting = Message.order('RANDOM()').first
     response_data = { greeting: greeting.greeting }
 
     json_response = JSON.pretty_generate(response_data)
